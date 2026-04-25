48f1cd279b2d20dfd24d6b97d74c3a6494a0b0e9
Udara Jayawardena <udarajayawardena5@gmail.com>
2026-04-26 02:19:08 +0530

    fix: cors-issue-fixed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173', // your Vite dev server
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
